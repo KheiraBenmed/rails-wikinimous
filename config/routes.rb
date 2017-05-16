e1ce47635eb6c413678379347b92fb4360744600
Kheira Benmeridja <kheira.benmeridja@gmail.com>
2017-05-16 14:43:09 +0200

    ajout controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  get 'articles/list'
+
+  get 'articles/show'
+
+  get 'articles/create'
+
+  get 'articles/destroy'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
